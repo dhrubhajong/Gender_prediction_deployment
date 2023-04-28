5b96d23b44f60e74723d36dc5a6016657380f930
Dhrub <dhrubhajong123@gmail.com>
2023-04-28 12:28:44 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,1 +1,13 @@
-import request
+import requests
+
+url = 'http://localhost:5000/predict_api'
+
+
+data = {'left canine width intraoral': left canine width intraoral,
+        'left canine width casts': left canine width casts,
+        'right canine width casts' : right canine width casts,
+        'right canine width intraoral': right canine width intraoral,
+        'intercanine distance casts' : intercanine distance casts,
+        'inter canine distance intraoral': inter canine distance intraoral}
+
+response= request.post(url,files=data)
